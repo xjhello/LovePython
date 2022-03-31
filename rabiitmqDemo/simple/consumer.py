0f5724bbe1aa7d02366ba20cfa920a5b0ecf402b
xujian <>
2022-03-31 13:08:00 +0800

    day day push

---
+++ rabiitmqDemo/simple/consumer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pika
+
+credentials = pika.PlainCredentials('admin', 'admin')
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='172.31.236.43', port=5672, virtual_host='/', credentials=credentials))
+channel = connection.channel()
+# 申明消息队列，消息在这个队列传递，如果不存在，则创建队列
+channel.queue_declare(queue='python-test', durable=False)
+
+
+# 定义一个回调函数来处理消息队列中的消息，这里是打印出来
+def callback(ch, method, properties, body):
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    print(body.decode())
+
+
+# 告诉rabbitmq，用callback来接收消息
+channel.basic_consume('python-test', callback)
+
+# 开始接收信息，并进入阻塞状态，队列里有信息才会调用callback进行处理
+channel.start_consuming()
--- rabiitmqDemo/simple/producter.py
+++ rabiitmqDemo/simple/producter.py
@@ -7,16 +7,15 @@ import json
 credentials = pika.PlainCredentials('admin', 'admin')  # mq用户名和密码
 # 虚拟队列需要指定参数 virtual_host，如果是默认的可以不填。
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.31.236.43', port=5672, virtual_host='/', credentials=credentials))
-
 channel = connection.channel()
 # 声明消息队列，消息将在这个队列传递，如不存在，则创建
 """
-      queue, 队列名称
-      passive=False,  
-      durable=False,  是否持久化
-      exclusive=False,  排他性，是否独立独占  
-      auto_delete=False,  是否自动删除  
-      arguments=None  携带附属参数
+  queue, 队列名称
+  passive=False,  
+  durable=False,  是否持久化
+  exclusive=False,  排他性，是否独立独占  
+  auto_delete=False,  是否自动删除  
+  arguments=None  携带附属参数
 """
 result = channel.queue_declare(queue='python-test')
 
