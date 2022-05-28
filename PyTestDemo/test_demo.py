22c425818e0a29f95d96c1c2efdd8d0c7a7a180d
xujian <>
2022-05-28 21:53:08 +0800

    day day push

---
+++ PyTestDemo/test_demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Test_demo():
+
+    def test_1(self):
+        print("test 1")
+        assert 1==1
+
+    def test_2(self):
+        print("test 2")
+        assert 2==2
+
+    def test_3(self):
+        print("test 3")
+        assert 3==2
+        # a = 1/0
